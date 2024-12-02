c6b3c18c60a30b7a3e1ecfec8d4a84ddd6fc39ae
paspartu22 <andreyovodov@yandex.ru>
2024-12-02 22:40:57 +0300

    day 2 unreadable

--- day02/main.py
+++ day02/main.py
@@ -1,5 +1,3 @@
-UP = 0
-DOWN = 1
 valid = [[1, 2, 3], [-1, -2, -3]]
 
 def solve(file_name):
@@ -10,36 +8,19 @@ def solve(file_name):
             report = [int(item) for item in line.split()]        
             part_1_result += check_report(report, 0, False, (report[1] - report[0] < 0))
             part_2_result += check_report(report, 0, True,  (report[1] - report[0] < 0))
-
+            
         print(part_1_result)
         print(part_2_result)
 
 def check_report(report, i,  can_dump, dir):
-    if i == len(report)-1:
-        return 1 #in the end
-    else:
-        if report[i+1] - report[i] in valid[dir]:
-            return check_report(report, i+1, can_dump, dir) #Next step
-                 
-        elif can_dump: #dumping cases
-
-            if (i <= 1 and check_report(report[1:], 0, False, (report[2]-report[1] < 0))):
-                print(report)
-                return 1
+    return  (i == len(report)-1 or                      ### end of report reached, all good
+            (report[i+1] - report[i] in valid[dir] and  ### ok, go to next step
+            check_report(report, i+1, can_dump, dir)) or
+            can_dump and(                               ### bad step, dumping
+            i <= 1 and check_report(report[1:], 0, False, (report[2]-report[1] < 0)) or                             #try drop first item
+            check_report(report[:i]+report[i+1:], i-1, False, dir if i > 1 else (report[i+1] - report[i-1] < 0)) or #try drop first item in conflict
+            check_report(report[:i+1]+report[i+2:], i, False, dir)))                                                #try drop second item in conflict
             
-            if check_report(report[:i]+report[i+1:], i-1, False, dir if i > 1 else (report[i+1] - report[i-1] < 0)):
-                print(report)
-                return 1
-            
-            if check_report(report[:i+1]+report[i+2:], i, False, dir):
-                print(report)
-                return 1
-            
-        return 0
-    
-        
-
-
 def main():
     solve('test.txt')
     solve('data.txt')
