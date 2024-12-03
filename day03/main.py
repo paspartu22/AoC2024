a95dc193452d2362a659596426bf5999b77ec5a0
Andrey Ovodov <a.ovodov@motorica.org>
2024-12-03 10:56:51 +0300

    day 03 additional

--- day03/main.py
+++ day03/main.py
@@ -2,52 +2,33 @@ import re
 
 
 def solve_part_1_re(file_name):
-    with open(file_name, 'r') as file:
-        print(f'{file_name} part 1')
-        print(sum(
-             int(re.split('\(|,|\)', item)[1]) 
-            *int(re.split('\(|,|\)', item)[2]) 
-            for item in 
-            re.findall('mul\(\d{,3},\d{,3}\)', file.readline())))
-
-
+    print(f'{file_name} part 1')
+    print(sum(
+          int(re.split('\(|,|\)', item)[1]) 
+         *int(re.split('\(|,|\)', item)[2]) 
+          for item in 
+          re.findall('mul\(\d{,3},\d{,3}\)', open(file_name, 'r').readline())))
 
 def solve_part_2_re(file_name):
     with open(file_name, 'r') as file:
         print(f'{file_name} part 2')
-        result = 0
-        line = file.readline()
 
-# 
         re_line = "don't\(\)|do\(\)|mul\(\d{,3},\d{,3}\)" 
-        x = re.findall(re_line, line)
+        line = file.readline()
         result = 0
         do = 1
-        for item in x:
-            print(item)
+        for item in re.findall(re_line, line):
+            #print(item)
             if (item == "do()"):
                 do = 1
             elif (item == "don't()"):
                 do = 0
             else:
+                item = re.split('\(|,|\)', item)
+                result += int(item[1])*int(item[2])*do
 
-                num1, num2 = item.split(',')
-                num1 = num1[4:]
-                num2 = num2[:-1]
-                #print (num1)
-                #print (num2)
-                result += int(num1)*int(num2)*do
-        #print(x)
         print(result)
 
-def get_number(line, start, ender):
-    for i in range(4):
-        item = line[start+i]
-        if i>0 and line[start+i] == ender:
-            return line[start:start+i]
-        elif not line[start+i].isdigit():
-            return -1 
-    return -1
 
 def main():
     solve_part_1_re('test.txt')
