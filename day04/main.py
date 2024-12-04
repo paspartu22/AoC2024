16ee2fd3e888bb592096f70a8e3f14def7dcefd5
Andrey Ovodov <andreyovodov@yandex.ru>
2024-12-04 14:56:06 +0300

    day 04

--- day04/main.py
+++ day04/main.py
@@ -1,7 +1,6 @@
-import re
-
-dirs = [[-1,-1], [-1,1], [1,-1], [1,1],
-        [1,0], [-1,0], [0,-1], [0,1]]
+dirs = [[-1,-1], [-1, 0], [-1, 1], 
+        [0 ,-1],          [0 , 1],
+        [1 ,-1], [1 , 0], [1 , 1]]
 STRING = "XMAS"
 
 def solve_part_1(file_name):
@@ -15,30 +14,20 @@ def solve_part_1(file_name):
             width = len(line)
             for x,letter in enumerate(line):
                 lines[y,x] = letter
-            print(line.strip())
+            #print(line.strip())
         result = 0
         for y in range(height):
             for x  in range(width):
                 if lines[y,x] == 'X':
                     for dir in dirs:
                         for i,search in enumerate(STRING[1:]):
-                            if (y+(dir[0]*(i+1)),x+(dir[1]*(i+1))) in lines:
-                                new_char = lines[y+(dir[0]*(i+1)),x+(dir[1]*(i+1))]
-                                if new_char  == search:
-                                    if new_char == STRING[-1]:
-                                        result += 1
-                                        #print(result)
-                                else:
-                                    break
+                            if ((y+(dir[0]*(i+1)),x+(dir[1]*(i+1))) in lines and 
+                                lines[y+(dir[0]*(i+1)),x+(dir[1]*(i+1))] == search):
+                                if lines[y+(dir[0]*(i+1)),x+(dir[1]*(i+1))] == STRING[-1]:
+                                    result += 1
                             else:
                                 break
         print(result)
-
-
-MAS = [{(-1, 1):'M', (1, -1) : 'S'},
-       {(1, -1):'M', (-1, 1) : 'S'},
-       {(1, 1) :'M', (-1, -1): 'S'},
-       {(-1,-1):'M', (1, 1)  : 'S'}]
 
 
 
@@ -53,23 +42,27 @@ def solve_part_2(file_name):
             width = len(line)
             for x,letter in enumerate(line):
                 lines[y,x] = letter
-            print(line.strip())
+            #print(line.strip())
         result = 0
         for y in range(height):
             for x  in range(width):
                 if lines[y,x] == 'A':
                     if check_mas(lines, y, x):
-                        print (f'{y},{x}')
                         result += 1
 
         print(result)
+        
+MAS = [{(-1, 1):'M', (1, -1) : 'S'},
+       {(1, -1):'M', (-1, 1) : 'S'},
+       {(1, 1) :'M', (-1, -1): 'S'},
+       {(-1,-1):'M', (1, 1)  : 'S'}]
 
 def check_mas(lines, y, x):
     result = 0
     for mas in MAS:
         for letter in mas.items():
-            if (y+letter [0][0], x+letter [0][1]) in lines:
-                new_char = lines[y+letter [0][0], x+letter [0][1]]
+            if (y+letter[0][0], x+letter[0][1]) in lines:
+                new_char = lines[y+letter[0][0], x+letter[0][1]]
                 if new_char != letter [1]:
                     break
                 elif new_char == 'S':
@@ -80,8 +73,8 @@ def check_mas(lines, y, x):
 
 
 def main():
-    #solve_part_1('test.txt')
-    #solve_part_1('data.txt')
+    solve_part_1('test.txt')
+    solve_part_1('data.txt')
     solve_part_2('test.txt')
     solve_part_2('data.txt')
 
